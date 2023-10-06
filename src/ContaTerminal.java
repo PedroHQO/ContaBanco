92e9dbc947a13eff16e3c8ebe82060ca08121130
PedroHQO <pedro39henrique.q.o@gmail.com>
2023-10-05 22:04:47 -0300

    Incluindo scanner

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class ContaTerminal{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        int num;
+        String agencia;
+        String nomeCliente;
+        Double saldo;
+
+        System.out.println("BEM VINDO AO MENU CRIACAO DE CONTA\n");
+        System.out.println("Informe o número da sua AGÊNCIA: ");
+        num = scanner.nextInt();
+        System.out.println("Agência: " + num);
+
     }
 }
